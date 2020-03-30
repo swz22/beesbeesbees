abde76bdf1659183c35fa4173fb6f928ed97a7d3
swz22 <jkim005@yahoo.com>
2020-03-30 13:45:46 -0700

    Completed HoneyMakerBee.js

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+  Bee.call(this); // creates pro
+  this.age = 10;
+  this.color = 'yellow';
+  this.job = 'make honey'
+  this.honeyPot = 0;
 };
+
+  HoneyMakerBee.prototype = Object.create(Bee.prototype); // creates
+  HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+  HoneyMakerBee.prototype.makeHoney = function () {
+    this.honeyPot++;
+  }
+
+  HoneyMakerBee.prototype.giveHoney = function () {
+    this.honeyPot--;
+  }
 
